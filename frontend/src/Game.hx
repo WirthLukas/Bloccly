eb8f1243ac8de0c610893924802310cec90c812f
Lukas Wirth <luke.wirth31@gmail.com>
2020-06-14 16:01:53 +0200

    refactoring and bug fixing

--- frontend/src/Game.hx
+++ frontend/src/Game.hx
@@ -21,22 +21,7 @@ using core.pattern.observer.ObservableExtender;
 using utils.ArrayTools;
 
 class Game extends hxd.App {
-
-    public static inline var FIELD_WIDTH = 10;
-    public static inline var FIELD_HEIGHT = 21;
-    public static inline var BLOCK_START_X = 5;
-    public static inline var BLOCK_START_Y = -2;
-
-    private var figure: Figure;
-    private var pool: BlockPool = new BlockPool();
-    private var tilePool: BlockTilePool = new BlockTilePool();
-    private var tf: Text;
-    private var i = 0;
     private var colorProvider: ColorProvidable = new LocalColorProvider();
-
-    private var updateCount: Int = 0;
-    private var resetCount: Int = 50;
-    private var nextColor: Color;
 
     //Multiplayer variables
     private var lost: Bool = false;
@@ -47,22 +32,9 @@ class Game extends hxd.App {
 
     public function new() {
         super();
-
-        // every time a new block is added to the game field
-        // this block will added to a block tile (which displays the block)
-        pool.onAdded = block -> tilePool
-            .getBlockTile(block, s2d)
-            .withColor(nextColor)
-            .setBlock(block)
-            .show();
-
-        pool.onFreed = block -> tilePool.freeOf(block);
-        nextColor = colorProvider.getNextColor();
     }
 
-    override function init() {
-        tf = new h2d.Text(hxd.res.DefaultFont.get(), s2d);
-        
+    override function init() {     
         /*var tf = new h2d.Text(hxd.res.DefaultFont.get(), s2d);
         tf.text = "Hello World !";
 
@@ -77,107 +49,25 @@ class Game extends hxd.App {
         g.beginFill(0xFF00FF, .5);
         g.drawCircle(200, 200, 100);*/
     
-        figure = newFigureOf(nextColor, BLOCK_START_X, BLOCK_START_Y);
+        // figure = newFigureOf(nextColor, BLOCK_START_X, BLOCK_START_Y);
 
-        ownPlayer = new OwnPlayer();
+        ownPlayer = new OwnPlayer(colorProvider, wsClient, s2d);
         ownPlayer.playerId = 1; //TODO: Get playerId through websocket
-        ownPlayer.initializeOwnPlayer(figure, pool, tilePool, tf, wsClient, colorProvider);
+        ownPlayer.init();
 
         players.push(ownPlayer);
-    }
-
-    private function log(text: String) {
-        tf.text = text;
     }
 
     override function update(dt:Float) {
         super.update(dt);
 
-        for(player in players)
-            player.draw();
+        // for(player in players)
+        //     player.update();
 
-        /*if (lost) return;
-
-        var blockReachedBottom = if (Key.isPressed(Key.SPACE)) {
-            while(!GameFieldChecker.checkBlockReachesBottom(figure.blocks, pool.usedBlocks)) {
-                figure.moveDown();
-            }
-
-            updateCount = 0;
-            true;
-        } else {
-            if (Key.isPressed(Key.DOWN)) {
-                figure.moveDown();
-            } else if (Key.isPressed(Key.LEFT)) {
-                if(GameFieldChecker.checkBlockCollision(figure.blocks, "left", pool.usedBlocks))
-                    figure.moveLeft();
-            } else if (Key.isPressed(Key.RIGHT)) {
-                if(GameFieldChecker.checkBlockCollision(figure.blocks, "right", pool.usedBlocks)){
-                    figure.moveRight();
-                }  
-            } else if (Key.isPressed(Key.UP)) {
-                figure.rotate();
-            } 
-
-            updateCount++;
-
-            if (updateCount == resetCount) {
-                updateCount = 0;
-                figure.moveDown();
-            }
-            
-            GameFieldChecker.checkBlockReachesBottom(figure.blocks, pool.usedBlocks);
-        } 
-
-        if(blockReachedBottom){
-            trace("Block reached Bottom");
-            if(figure.blocks.filter(block -> block.y < 0).length > 0){
-                lost = true;
-                tf.text = "You lost the game.";
-                trace("You lost the game.");
-                var wsMessage = new WebSocketMessage(CommandType.Loss, playerId, pool.usedBlocks);
-                wsClient.sendWebSocketMessage(wsMessage);
-            }
-            else {
-                //If a Block reaches the end of its journey, checkRowFull() is called to check, if it filled a line
-                var fullRows: Array<Bool> = GameFieldChecker.checkRowFull(pool.usedBlocks);
-    
-                if (fullRows.map(row -> row).length >= 1) {
-                    clearFullRows(fullRows); 
-                }
-                
-                var wsMessage = new WebSocketMessage(CommandType.BlockUpdate, playerId, pool.usedBlocks);
-                wsClient.sendWebSocketMessage(wsMessage);
-    
-                //Create new Figure and notify wsClient
-                nextColor = colorProvider.getNextColor();
-                figure = newFigureOf(nextColor, BLOCK_START_X, BLOCK_START_Y);
-                var wsMessage = new WebSocketMessage(CommandType.NewBlock, playerId, figure.blocks);
-                wsClient.sendWebSocketMessage(wsMessage);
-            }
-        }*/
+        players.forEach(p -> p.update());
     }
 
-    private function newFigureOf(color: view.Color, x: Int, y: Int): Figure
-        return switch color {
-            case Orange: FigureBuilder.getOrange(pool, x, y);
-            case Cyan: FigureBuilder.getCyan(pool, x, y);
-            case Blue: FigureBuilder.getBlue(pool, x, y);
-            case Green: FigureBuilder.getGreen(pool, x, y);
-            case Purple: FigureBuilder.getPurple(pool, x, y);
-            case Red: FigureBuilder.getRed(pool, x, y);
-            case Yellow: FigureBuilder.getYellow(pool, x, y);
-            default: throw "No such type available";
-        }
-
-    /*private inline function clearFullRows(fullRows: Array<Bool>): Void
-        for(i in 0...fullRows.length)
-            if(fullRows[i]) {
-                trace('free row $i');
-                pool.freeRow(i);
-                pool.moveAllBlocksAboveRow(i, 0, 1);
-            }
-                
+    /*            
     //Needs more functionality
     private function startNewGame(){
         var fullRows: Array<Bool> = [ for(i in 0...FIELD_HEIGHT) true];
